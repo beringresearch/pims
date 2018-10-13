8fa38714b9db85373ef8f60ff496e47aca422b80
Ignat Drozdov <idrozdov@beringresearch.com>
2018-10-13 15:34:15 +0100

    better centering

--- pims.py
+++ pims.py
@@ -21,8 +21,10 @@ app.layout = html.Div([
     dcc.Dropdown(id = 'live-dropdown',
                  options=[{'label': i, 'value': i} for i in list_of_images],
                  value=list_of_images[0],
-                 clearable=False), 
-    html.Img(id='image', style={'width': 750, 'align': 'center'}),
+                 clearable=False),
+    html.Div(
+        html.Img(id='image', style={'width': 750}),
+        style={'width': 750, 'align': 'center', 'margin': '0 auto', 'margin-top': 25}),
     dcc.Interval(id='interval-component',
                  interval=1*10000, # in milliseconds
                  n_intervals=60)],
@@ -34,9 +36,7 @@ app.layout = html.Div([
         [Input('interval-component', 'n_intervals')])
 def update_dropdown(n): 
     list_of_images = [os.path.basename(x) for x in glob.glob('{}*.png'.format(image_directory))]
-    options = [{'label': i, 'value': i} for i in list_of_images]
-    print(list_of_images, file = sys.stderr)
-    print(image_directory) 
+    options = [{'label': i, 'value': i} for i in list_of_images] 
     for f in list_of_images:
         flask.send_from_directory(image_directory, f)
 
@@ -49,7 +49,7 @@ def update_dropdown(n):
 def update_image_src(selected_value):
     return static_image_route + selected_value
 
-# Add a static image route that serves images from desktop
+# Add a static image route that serves images from directory
 # Be *very* careful here - you don't want to serve arbitrary files
 # from your computer or server
 @app.server.route('{}<image_path>.png'.format(static_image_route))
