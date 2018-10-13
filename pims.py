c33404bd5436ad68fa7a52d17bf5f303e31e9ca9
Ignat Drozdov <idrozdov@beringresearch.com>
2018-10-13 22:06:06 +0100

    update caching and live serving without static folder

--- pims.py
+++ pims.py
@@ -7,17 +7,16 @@ import dash_html_components as html
 
 from dash.dependencies import Input, Output
 
-import flask
 import glob
 import os
+import base64
 
 image_directory = sys.argv[1]
 list_of_images = [os.path.basename(x) for x in glob.glob('{}*.png'.format(image_directory))]
-static_image_route = '/static/'
 
 app = dash.Dash()
 
-app.layout = html.Div([
+app.layout =  html.Div([
     dcc.Dropdown(id = 'live-dropdown',
                  options=[{'label': i, 'value': i} for i in list_of_images],
                  value=list_of_images[0],
@@ -31,10 +30,11 @@ app.layout = html.Div([
     style={'width': 800, 'align': 'center', 'margin': '0 auto'})
 
 
+
 @app.callback(
         Output('live-dropdown', 'options'),
         [Input('interval-component', 'n_intervals')])
-def update_dropdown(n):
+def update_dropdown(n): 
     global list_of_images
     list_of_images = [os.path.basename(x) for x in glob.glob('{}*.png'.format(image_directory))]
     options = [{'label': i, 'value': i} for i in list_of_images] 
@@ -46,17 +46,11 @@ def update_dropdown(n):
     Output('image', 'src'),
     [Input('live-dropdown', 'value')])
 def update_image_src(selected_value):
-    return static_image_route + selected_value
+    image_filename =  os.path.join(image_directory, selected_value)
+    encoded_image = base64.b64encode(open(image_filename, 'rb').read())
+    return 'data:image/png;base64,{}'.format(encoded_image.decode())
+    #return static_image_route + selected_value
 
-# Add a static image route that serves images from directory
-# Be *very* careful here - you don't want to serve arbitrary files
-# from your computer or server
-@app.server.route('{}<image_path>.png'.format(static_image_route))
-def serve_image(image_path):
-    image_name = '{}.png'.format(image_path)
-    if image_name not in list_of_images:
-        raise Exception('"{}" is excluded from the allowed static files'.format(image_path))
-    return flask.send_from_directory(image_directory, image_name)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
