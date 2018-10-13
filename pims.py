29edf24dc9ae07ba5c08c7e27ab0d1f064d2ec70
Ignat Drozdov <idrozdov@beringresearch.com>
2018-10-13 15:17:08 +0100

    dynamically refresh image list

--- pims.py
+++ pims.py
@@ -5,6 +5,8 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
+from dash.dependencies import Input, Output
+
 import flask
 import glob
 import os
@@ -16,19 +18,36 @@ static_image_route = '/static/'
 app = dash.Dash()
 
 app.layout = html.Div([
-    dcc.Dropdown(
-        id='image-dropdown',
-        options=[{'label': i, 'value': i} for i in list_of_images],
-        value=list_of_images[0], style={'font': 'arial'}
-    ),
-    html.Img(id='image', style={'width': 750, 'align': 'center'})
-], style={'width': 800, 'align': 'center', 'margin': '0 auto'})
+    dcc.Dropdown(id = 'live-dropdown',
+                 options=[{'label': i, 'value': i} for i in list_of_images],
+                 value=list_of_images[0],
+                 clearable=False), 
+    html.Img(id='image', style={'width': 750, 'align': 'center'}),
+    dcc.Interval(id='interval-component',
+                 interval=1*10000, # in milliseconds
+                 n_intervals=60)],
+    style={'width': 800, 'align': 'center', 'margin': '0 auto'})
+
 
 @app.callback(
-    dash.dependencies.Output('image', 'src'),
-    [dash.dependencies.Input('image-dropdown', 'value')])
-def update_image_src(value):
-    return static_image_route + value
+        Output('live-dropdown', 'options'),
+        [Input('interval-component', 'n_intervals')])
+def update_dropdown(n): 
+    list_of_images = [os.path.basename(x) for x in glob.glob('{}*.png'.format(image_directory))]
+    options = [{'label': i, 'value': i} for i in list_of_images]
+    print(list_of_images, file = sys.stderr)
+    print(image_directory) 
+    for f in list_of_images:
+        flask.send_from_directory(image_directory, f)
+
+    return options
+
+
+@app.callback(
+    Output('image', 'src'),
+    [Input('live-dropdown', 'value')])
+def update_image_src(selected_value):
+    return static_image_route + selected_value
 
 # Add a static image route that serves images from desktop
 # Be *very* careful here - you don't want to serve arbitrary files
